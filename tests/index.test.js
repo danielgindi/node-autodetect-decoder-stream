d8554b1f6df33a8689c33b3fd9fa8a42207240a3
Daniel Cohen Gindi <danielgindi@gmail.com>
2018-12-17 10:04:37 +0200

    Moved tests to mocha to reduce deps tree, faster install

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,12 +1,13 @@
 const Iconv = require('iconv-lite');
 const StreamBuffers = require('stream-buffers');
 const AutoDetectDecoderStream = require('../');
+const assert = require('assert');
 
 describe('Autodetect Detector Stream', () => {
     let stream;
     let result;
 
-    beforeAll(() => {
+    before(() => {
         Iconv.getCodec('ascii'); // Force lazy loading of encoding before tests, otherwise it fails
     });
 
@@ -15,7 +16,7 @@ describe('Autodetect Detector Stream', () => {
         result = '';
     });
 
-    test('Basic ASCII encoding', (done) => {
+    it('Basic ASCII encoding', (done) => {
         const buffer = Buffer.from([0x54, 0x65, 0x73, 0x74]);
         stream
             .pipe(new AutoDetectDecoderStream({defaultEncoding: 'ascii'}))
@@ -23,14 +24,14 @@ describe('Autodetect Detector Stream', () => {
                 result += data;
             })
             .on('end', () => {
-                expect(result).toEqual('Test');
+                assert.strictEqual(result, 'Test');
                 done();
             });
         stream.put(buffer);
         stream.stop();
     });
 
-    test('No detection fallback to default encoding', (done) => {
+    it('No detection fallback to default encoding', (done) => {
         const buffer = Buffer.from([0xBF, 0x54, 0x65, 0x73, 0x74]);
         stream
             .pipe(new AutoDetectDecoderStream({defaultEncoding: 'utf8', minConfidence: 1}))
@@ -38,14 +39,14 @@ describe('Autodetect Detector Stream', () => {
                 result += data;
             })
             .on('end', () => {
-                expect(result).toEqual('�Test');
+                assert.strictEqual(result, '�Test');
                 done();
             });
         stream.put(buffer);
         stream.stop();
     });
 
-    test('Empty stream returns empty data', (done) => {
+    it('Empty stream returns empty data', (done) => {
         const buffer = Buffer.alloc(0);
         stream
             .pipe(new AutoDetectDecoderStream({defaultEncoding: 'ascii'}))
@@ -53,45 +54,45 @@ describe('Autodetect Detector Stream', () => {
                 result += data;
             })
             .on('end', () => {
-                expect(result).toEqual('');
+                assert.strictEqual(result, '');
                 done();
             });
         stream.put(buffer);
         stream.stop();
     });
 
-    test('Unknown encoding emits error', (done) => {
+    it('Unknown encoding emits error', (done) => {
         const buffer = Buffer.alloc(0);
         stream
             .pipe(new AutoDetectDecoderStream({defaultEncoding: 'cp99999'}))
             .on('error', (err) => {
-                expect(err).not.toBeNull();
+                assert.notStrictEqual(err, null);
                 done();
             });
         stream.put(buffer);
         stream.stop();
     });
 
-    test('Collect callback', (done) => {
+    it('Collect callback', (done) => {
         const buffer = Buffer.from([0x54, 0x65, 0x73, 0x74]);
         stream
             .pipe(new AutoDetectDecoderStream({defaultEncoding: 'ascii'}))
             .collect((err, body) => {
-                expect(err).toBeNull();
-                expect(body).toEqual('Test');
+                assert.strictEqual(err, null);
+                assert.strictEqual(body, 'Test');
                 done();
             });
         stream.put(buffer);
         stream.stop();
     });
 
-    test('Collect callback with error', (done) => {
+    it('Collect callback with error', (done) => {
         const buffer = Buffer.alloc(0);
         stream
             .pipe(new AutoDetectDecoderStream({defaultEncoding: 'cp99999'}))
             .collect((err, body) => {
-                expect(err).not.toBeNull();
-                expect(body).toBeUndefined();
+                assert.notStrictEqual(err, null);
+                assert.strictEqual(body, undefined);
                 done();
             });
         stream.put(buffer);
